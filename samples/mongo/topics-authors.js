6493d88646837ff2bc3ed6ea48ee72d593851900
Meric Melike Softa <msofta@ku.edu.tr>
2017-04-21 02:51:30 +0300

    topic authors

---
+++ samples/mongo/topics-authors.js
@@ -0,0 +1,30 @@
+/**
+ * Created by melikesofta on 21.04.2017.
+ */
+
+COLLECTION_NAME  = "entry";                 // Full Set 30m Entry
+COLLECTION_SAMPLE_NAME  = "entry_sample";   // Sample set 100k Entry
+
+db = new Mongo().getDB("eksi-db");
+collection = db.getCollection(COLLECTION_SAMPLE_NAME);
+
+db.getCollection("topic_authors").drop()
+db.createCollection("topic_authors")
+db.topic_authors.insert(
+    db.entry_sample.aggregate(
+        [
+            {
+                $group: {_id:"$title.title", authors: {$addToSet: "$author.name"}}
+            },
+            {
+                $project: {"authors": "$authors", "size": {$size: "$authors"}}
+            },
+            {
+                $sort: {"size": -1},
+            }
+        ],
+        {allowDiskUse: true}
+    ).toArray()
+)
+
+//db.topic_authors.remove({"authors": {$exists:true}, $where: 'this.authors.length<20'})
