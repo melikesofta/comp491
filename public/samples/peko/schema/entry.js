36cc7b07afa5e2c03f6979421ed4c5e538faeb1d
Ahmet Bingul <abingu13@ku.edu.tr>
2017-05-05 00:09:01 +0300

    peko

---
+++ public/samples/peko/schema/entry.js
@@ -0,0 +1,79 @@
+/**
+ *
+ * DO NOT PUT CODE HERE
+ *
+ */
+
+
+ENTRY = {
+    /**
+     *
+     * Identifier
+     *
+     */
+    "_id": ObjectId("589c5d8309485a31f8d9c2c4"),
+    "id": 209069,
+
+    /**
+     *
+     * Irrelavant
+     *
+     */
+    "url": "https://eksisozluk.com/entry/209069",
+    "favorite_count": 0,
+    "status": 1,
+    "content": "seneler once trt izmir'in hazirlayip sundugu cumartesi oglen eglence programi",
+
+    /**
+     *
+     * Topic
+     *
+     */
+    "topic_id": 83259,
+    "name_normalized": "imbat",
+    "title": {
+        "title": "imbat",
+        "url": "https://eksisozluk.com/imbat--83259",
+        "id": "83259"
+    },
+
+    /**
+     *
+     * Hrefs
+     *
+     */
+    "href": [
+        "trt",
+        "izmir",
+        "cumartesi"
+    ],
+    "href_norms": [
+        "trt",
+        "izmir",
+        "cumartesi"
+    ],
+
+    /**
+     *
+     * Author
+     *
+     */
+    "author_id": 7222,
+    "author": {
+        "url": "https://eksisozluk.com/biri/bloody",
+        "name": "bloody",
+        "id": "7222"
+    },
+
+    /**
+     *
+     * Date
+     *
+     */
+    "month": 6,
+    "year": 2000,
+    "date_normalized": ISODate("2000-06-22T00:00:00Z"),
+    "date": "22.06.2000 01:12",
+
+
+}
